8a7396bb8a87acf0178b1d7e67611e20408f985e
Thies Verhave <thies@verhave.net>
2022-11-07 11:12:13 +0100

    more unit tests

---
+++ tests/Unit/Service/JsonWebTokenServiceTest.php
@@ -0,0 +1,263 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JsonWebToken\Tests\Unit\Service;
+
+use JsonException;
+use JsonWebToken\Entity\DecodedToken;
+use JsonWebToken\Exception\AlgorithmNotSupported;
+use JsonWebToken\Exception\HeaderNotFoundException;
+use JsonWebToken\Exception\ValidatorNotFound;
+use JsonWebToken\Service\JsonWebTokenService;
+use PHPUnit\Framework\TestCase;
+use function file_get_contents;
+use function sprintf;
+
+final class JsonWebTokenServiceTest extends TestCase
+{
+    private function getPrivateKey(): string
+    {
+        return file_get_contents(sprintf('%s/../../Fixtures/Encoder/RSA/privatekey', __DIR__));
+    }
+
+    private function getPublicKey(): string
+    {
+        return file_get_contents(sprintf('%s/../../Fixtures/Decoder/RSA/publickey', __DIR__));
+    }
+
+    public function encodeDataProvider(): array
+    {
+        return [
+            [
+                [
+                    'alg' => 'HS256',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'iat' => 1516239022
+                ],
+                'your-256-bit-secret',
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+            ],
+            [
+                [
+                    'alg' => 'HS384',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'admin' => true,
+                    'iat' => 1516239022
+                ],
+                'your-384-bit-secret',
+                'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.bQTnz6AuMJvmXXQsVPrxeQNvzDkimo7VNXxHeSBfClLufmCVZRUuyTwJF311JHuh'
+            ],
+            [
+                [
+                    'alg' => 'HS512',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'admin' => true,
+                    'iat' => 1516239022
+                ],
+                'your-512-bit-secret',
+                'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.VFb0qJ1LRg_4ujbZoRMXnVkUgiuKq5KxWqNdbKq_G9Vvz-S1zZa9LPxtHWKa64zDl2ofkT8F6jBt_K4riU-fPg'
+            ],
+            [
+                [
+                    'alg' => 'RS256',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'admin' => true,
+                    'iat' => 1516239022
+                ],
+                $this->getPrivateKey(),
+                'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ'
+            ],
+            [
+                [
+                    'alg' => 'RS384',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'admin' => true,
+                    'iat' => 1516239022
+                ],
+                $this->getPrivateKey(),
+                'eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.o1hC1xYbJolSyh0-bOY230w22zEQSk5TiBfc-OCvtpI2JtYlW-23-8B48NpATozzMHn0j3rE0xVUldxShzy0xeJ7vYAccVXu2Gs9rnTVqouc-UZu_wJHkZiKBL67j8_61L6SXswzPAQu4kVDwAefGf5hyYBUM-80vYZwWPEpLI8K4yCBsF6I9N1yQaZAJmkMp_Iw371Menae4Mp4JusvBJS-s6LrmG2QbiZaFaxVJiW8KlUkWyUCns8-qFl5OMeYlgGFsyvvSHvXCzQrsEXqyCdS4tQJd73ayYA4SPtCb9clz76N1zE5WsV4Z0BYrxeb77oA7jJhh994RAPzCG0hmQ'
+            ],
+            [
+                [
+                    'alg' => 'RS512',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'admin' => true,
+                    'iat' => 1516239022
+                ],
+                $this->getPrivateKey(),
+                'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.jYW04zLDHfR1v7xdrW3lCGZrMIsVe0vWCfVkN2DRns2c3MN-mcp_-RE6TN9umSBYoNV-mnb31wFf8iun3fB6aDS6m_OXAiURVEKrPFNGlR38JSHUtsFzqTOj-wFrJZN4RwvZnNGSMvK3wzzUriZqmiNLsG8lktlEn6KA4kYVaM61_NpmPHWAjGExWv7cjHYupcjMSmR8uMTwN5UuAwgW6FRstCJEfoxwb0WKiyoaSlDuIiHZJ0cyGhhEmmAPiCwtPAwGeaL1yZMcp0p82cpTQ5Qb-7CtRov3N4DcOHgWYk6LomPR5j5cCkePAz87duqyzSMpCB0mCOuE3CU2VMtGeQ'
+            ],
+        ];
+    }
+
+    public function decodeDataProvider(): array
+    {
+        return [
+            [
+                [
+                    'alg' => 'HS256',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'iat' => 1516239022
+                ],
+                'your-256-bit-secret',
+                'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+            ],
+            [
+                [
+                    'alg' => 'HS384',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'admin' => true,
+                    'iat' => 1516239022
+                ],
+                'your-384-bit-secret',
+                'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.bQTnz6AuMJvmXXQsVPrxeQNvzDkimo7VNXxHeSBfClLufmCVZRUuyTwJF311JHuh'
+            ],
+            [
+                [
+                    'alg' => 'HS512',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'admin' => true,
+                    'iat' => 1516239022
+                ],
+                'your-512-bit-secret',
+                'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.VFb0qJ1LRg_4ujbZoRMXnVkUgiuKq5KxWqNdbKq_G9Vvz-S1zZa9LPxtHWKa64zDl2ofkT8F6jBt_K4riU-fPg'
+            ],
+            [
+                [
+                    'alg' => 'RS256',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'admin' => true,
+                    'iat' => 1516239022
+                ],
+                $this->getPublicKey(),
+                'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.NHVaYe26MbtOYhSKkoKYdFVomg4i8ZJd8_-RU8VNbftc4TSMb4bXP3l3YlNWACwyXPGffz5aXHc6lty1Y2t4SWRqGteragsVdZufDn5BlnJl9pdR_kdVFUsra2rWKEofkZeIC4yWytE58sMIihvo9H1ScmmVwBcQP6XETqYd0aSHp1gOa9RdUPDvoXQ5oqygTqVtxaDr6wUFKrKItgBMzWIdNZ6y7O9E0DhEPTbE9rfBo6KTFsHAZnMg4k68CDp2woYIaXbmYTWcvbzIuHO7_37GT79XdIwkm95QJ7hYC9RiwrV7mesbY4PAahERJawntho0my942XheVLmGwLMBkQ'
+            ],
+            [
+                [
+                    'alg' => 'RS384',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'admin' => true,
+                    'iat' => 1516239022
+                ],
+                $this->getPublicKey(),
+                'eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.o1hC1xYbJolSyh0-bOY230w22zEQSk5TiBfc-OCvtpI2JtYlW-23-8B48NpATozzMHn0j3rE0xVUldxShzy0xeJ7vYAccVXu2Gs9rnTVqouc-UZu_wJHkZiKBL67j8_61L6SXswzPAQu4kVDwAefGf5hyYBUM-80vYZwWPEpLI8K4yCBsF6I9N1yQaZAJmkMp_Iw371Menae4Mp4JusvBJS-s6LrmG2QbiZaFaxVJiW8KlUkWyUCns8-qFl5OMeYlgGFsyvvSHvXCzQrsEXqyCdS4tQJd73ayYA4SPtCb9clz76N1zE5WsV4Z0BYrxeb77oA7jJhh994RAPzCG0hmQ'
+            ],
+            [
+                [
+                    'alg' => 'RS512',
+                    'typ' => 'JWT'
+                ],
+                [
+                    'sub' => '1234567890',
+                    'name' => 'John Doe',
+                    'admin' => true,
+                    'iat' => 1516239022
+                ],
+                $this->getPublicKey(),
+                'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.jYW04zLDHfR1v7xdrW3lCGZrMIsVe0vWCfVkN2DRns2c3MN-mcp_-RE6TN9umSBYoNV-mnb31wFf8iun3fB6aDS6m_OXAiURVEKrPFNGlR38JSHUtsFzqTOj-wFrJZN4RwvZnNGSMvK3wzzUriZqmiNLsG8lktlEn6KA4kYVaM61_NpmPHWAjGExWv7cjHYupcjMSmR8uMTwN5UuAwgW6FRstCJEfoxwb0WKiyoaSlDuIiHZJ0cyGhhEmmAPiCwtPAwGeaL1yZMcp0p82cpTQ5Qb-7CtRov3N4DcOHgWYk6LomPR5j5cCkePAz87duqyzSMpCB0mCOuE3CU2VMtGeQ'
+            ],
+        ];
+    }
+
+    /**
+     * @throws HeaderNotFoundException
+     * @throws AlgorithmNotSupported
+     */
+    public function testEncodeAlgorithmHeaderNotFound(): void
+    {
+        $this->expectException(HeaderNotFoundException::class);
+
+        $service = new JsonWebTokenService();
+        $service->encode([], [], '');
+    }
+
+    /**
+     * @dataProvider encodeDataProvider
+     *
+     * @throws AlgorithmNotSupported
+     * @throws HeaderNotFoundException
+     */
+    public function testEncodeSuccessful(array $header, array $payload, string $secret, string $expectedToken): void
+    {
+        $service = new JsonWebTokenService();
+        $token = $service->encode($header, $payload, $secret);
+
+        $this->assertEquals($expectedToken, $token);
+    }
+
+    /**
+     * @throws AlgorithmNotSupported
+     * @throws HeaderNotFoundException
+     */
+    public function testDecodeAlgorithmNotFound(): void
+    {
+        $this->expectException(HeaderNotFoundException::class);
+
+        $service = new JsonWebTokenService();
+        $service->encode([], [], '');
+    }
+
+    /**
+     * @dataProvider decodeDataProvider
+     *
+     * @throws AlgorithmNotSupported
+     * @throws HeaderNotFoundException
+     * @throws JsonException
+     * @throws ValidatorNotFound
+     */
+    public function testDecodeSuccessful(array $header, array $payload, string $secret, string $token): void
+    {
+        $service = new JsonWebTokenService();
+        $decodedToken = $service->decode($token, $secret);
+
+        $this->assertInstanceOf(DecodedToken::class, $decodedToken);
+        $this->assertTrue($decodedToken->isValid());
+        $this->assertEquals($header, $decodedToken->getHeader());
+        $this->assertEquals($payload, $decodedToken->getPayload());
+    }
+}
